0860f1237c34e8d014488566c5eb60074813b8dc
yannay <yannayl+uberclock@gmail.com>
2014-05-06 16:27:09 +0300

    returned to the origina get deps script

--- get-pacman-dependencies.sh
+++ get-pacman-dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e -u -o pipefail
 
 shared_dependencies() {
@@ -8,68 +8,17 @@ shared_dependencies() {
   done | awk '{print $5}'
 }
 
-pkgbuild_prvided() {
-  local PKGBUILD=$1
-  provides=
-  source "$PKGBUILD"
-  [[ -z "$provides" ]] && return
-  dep_list=
-  echo "${provides[@]}"
-}
-
 pkgbuild_dependencies() {
   local PKGBUILD=$1
-  local EXCLUDE="$2"
-  depends=
+  local EXCLUDE=$2
   source "$PKGBUILD"
-  [[ -z "$depends" ]] && return
-  dep_list=
   for DEPEND in ${depends[@]}; do
-	[[ -n "$DEPEND" ]] || continue
-    dep=`echo "$DEPEND" | sed "s/[>=<].*$//"`
-	[[ -n "$dep" ]] || continue 
-	echo "$EXCLUDE" | grep -wq "$dep" && continue
-	dep_list="$dep_list $dep"
-  done
-
-  echo "$dep_list"
-}
-
-download_pkgbuild() {
-	local PACKAGE=$1
-	local PKGBUILD_TARGET=$2
-
-	rsync rsync://rsync.archlinux.org/abs/i686/core/$PACKAGE/PKGBUILD $PKGBUILD_TARGET ||\
-		rsync rsync://rsync.archlinux.org/abs/any/core/$PACKAGE/PKGBUILD $PKGBUILD_TARGET
+    echo "$DEPEND" | sed "s/[>=<].*$//"
+  done | grep -v "$EXCLUDE"
 }
 
 # Main
-{
-  
-#  shared_dependencies "/usr/bin/pacman"
-  pkg_list=
-  pkg_provided_list=
-  added_list=pacman
-#  pkgbuild_dir=`mktemp -d`
-  pkgbuild_dir=/tmp/pkgbuild
-  mkdir -p  "$pkgbuild_dir"
-  echo "pkgbuild_dir=$pkgbuild_dir"
-  while [[ -n "$added_list" ]]; do
-	pkg_list="$pkg_list $added_list"
-	echo "current pkglist is $pkg_list"
-	newly_added_list="$added_list"
-	echo "newly_added_list is $newly_added_list"
-	added_list=
-
-	for pkg in $newly_added_list; do
-	  echo "checking pkg $pkg"
-	  download_pkgbuild "$pkg" "$pkgbuild_dir/$pkg"
-      pkg_provided_list="$pkg_provided_list `pkgbuild_prvided "$pkgbuild_dir/$pkg"`"
-      added_list="$added_list `pkgbuild_dependencies "$pkgbuild_dir/$pkg" " $pkg_list $added_list $pkg_provided_list"` "
-	done
-
-  	added_list=`echo -n $added_list | sed '/^$/d' | sort | uniq`
-  done
-
-  echo "$pkg_list"
-}
+{ 
+  shared_dependencies "/usr/bin/pacman"
+  pkgbuild_dependencies "/var/abs/core/pacman/PKGBUILD" "bash"
+} | sort -u | xargs
